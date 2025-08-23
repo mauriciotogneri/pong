654489bd898b63c4ddfeef20efe18480340c96c5
Mauricio Togneri <mauricio.togneri@gmail.com>
2025-08-23 19:52:46 +0200

    Improvements

--- frontend/lib/models/database.dart
+++ frontend/lib/models/database.dart
@@ -1,11 +1,14 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:pong/json/json_session.dart';
 import 'package:pong/models/description.dart';
+import 'package:pong/types/session_status.dart';
 
 class Database {
+  static CollectionReference get collection =>
+      FirebaseFirestore.instance.collection('connections');
+
   static Future<Description?> getExistingOffer() async {
-    final QuerySnapshot snapshot = await FirebaseFirestore.instance
-        .collection('connections')
+    final QuerySnapshot snapshot = await collection
         .where('status', isEqualTo: 'offered')
         .limit(1)
         .get();
@@ -21,11 +24,19 @@ class Database {
     }
   }
 
-  static Future createOffer(Description description) =>
-      FirebaseFirestore.instance.collection('connections').add({
-        'sdp': description.sdp,
-        'type': description.type,
-        'status': 'offered',
-        'timestamp': FieldValue.serverTimestamp(),
-      });
+  static Future createSession(Description description) async {
+    final JsonSession session = JsonSession(
+      createdAt: DateTime.now(),
+      callerDescription: JsonDescription(
+        sdp: description.sdp,
+        type: description.type,
+      ),
+      calleeDescription: null,
+      callerCandidates: [],
+      calleeCandidates: [],
+      status: SessionStatus.offered,
+    );
+
+    await collection.add(session.toJson());
+  }
 }
